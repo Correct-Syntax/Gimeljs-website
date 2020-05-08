ac9858e3c815f51751eb7249c43dc625543fcd66
Correct Syntax <correctsyntax@yahoo.com>
2020-05-08 11:04:35 -0500

    added source

---
+++ src/_site/assets/js/main.js
@@ -0,0 +1,17 @@
+$g.fadeIn("#heroHeader", "translate", "(-14rem, -4rem)");
+$g.fadeIn(".code-block", "rotate3d", "(2, 14, 5, -300deg)");
+
+$g.when("window", "load", function() {
+    $g.fadeInStart("#heroHeader", "translate", 1.1);
+    $g.whenTimeUp(500, function() {
+        $g.typingEffect(
+            "#heroSubheader", 
+            "Free & Open-Source Javascript Library for DOM Manipulation & Effects",
+            30
+        );
+    });
+});
+
+$g.whenScrollTo("#includejs", function() {
+  $g.fadeInStart(".code-block" , "rotate3d", 2);
+});
